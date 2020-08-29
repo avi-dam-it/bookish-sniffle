621007e47955ba0402bc47fc3b853170e6cff7a0
Avaneesh Jai Damaraju <avidam@MacBook-Air.local>
2020-08-29 17:36:44 +0530

    added file

---
+++ avaneesh.cpp
@@ -0,0 +1,34 @@
+//binary search from scratch
+#include <iostream>
+
+using namespace std;
+
+int sort(int *a, int n)
+{
+  //bubble sort
+  int i;
+  for(i=0; i<n; i++)
+  {
+    if(a[i]>a[i+1])
+    {
+      
+    }
+  }
+}
+
+int main()
+{
+  int a[100], n, i, key;
+  cout<<"Enter number of elements: "<<endl;
+  cin>>n;
+
+  cout<<"Enter array elements"<<endl;
+  for(i=0; i<n; i++)
+  {
+    cin>>a[i];
+  }
+  //binary search begins
+  cout<< "Enter element to be searched:"<<endl;
+  cin>>key;
+
+}
